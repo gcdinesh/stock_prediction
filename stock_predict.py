3adc338ff69e76fd928c65934629f476cfd59057
Dinesh <gcdinesh14@gmail.com>
2021-05-21 00:49:27 +0530

    sort the data date wise

--- stock_predict.py
+++ stock_predict.py
@@ -3,16 +3,17 @@ import numpy as np
 import mplcursors
 import matplotlib.pyplot as plt
 from matplotlib.pylab import rcParams
-%matplotlib inline
+#%matplotlib inline
 # use %matplotlib qt instead of inline to visualize and hover the graph, but it is too slow
+#instead of using this command here go to tools-> preferences -> ipython console -> graphics -> backend(dropdown) to inline
 
 rcParams['figure.figsize'] = 20,10
 
 df = pd.read_csv('C:/Users/dchandra/Downloads/Mine/Study/Projects/stock_prediction/tataglobal.csv')
+df = df.sort_values('Date')
 df.index = df['Date']
 print(df.head())
 
 plt.plot(df['Close'], label='Close Price history')
-plt.gca().invert_xaxis()
 mplcursors.cursor(hover=True)
 plt.show()
