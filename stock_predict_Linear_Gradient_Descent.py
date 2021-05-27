10e81773b359559bd727e01917ad01aeb3a52779
Dinesh <gcdinesh14@gmail.com>
2021-05-28 00:27:45 +0530

    Some refactoring, tried normalization but not working

--- stock_predict_Linear_Gradient_Descent.py
+++ stock_predict_Linear_Gradient_Descent.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import mplcursors
 import matplotlib.pyplot as plt
 from matplotlib.pylab import rcParams
 from pandas import DataFrame
@@ -10,15 +9,21 @@ from fastai.tabular.all import add_datepart
 #instead of using this command here go to tools-> preferences -> ipython console -> graphics -> backend(dropdown) to inline
 
 rcParams['figure.figsize'] = 20,10
-
+pd.set_option('display.max_columns', None)
 df1 = pd.read_csv('C:/Users/dchandra/Downloads/Mine/Study/Projects/stock_prediction/tataglobal.csv')
 df = DataFrame(df1)
 df = df.sort_values('Date')
 df.index = df['Date']
 df = add_datepart(df, 'Date')
-df = df[['Dayofweek', 'Close']]
 actualVals = df['Close'].tolist()
 actualDayOfWeekVals = df['Dayofweek']
+df.reset_index(drop = True, inplace=True)
+df = df.drop(['Open', 'High', 'Low', 'Last', 'Total Trade Quantity', 'Turnover (Lacs)', 'Elapsed', 'Is_month_end',
+'Is_month_start',
+'Is_quarter_end',
+'Is_quarter_start',
+'Is_year_end',
+'Is_year_start'], axis = 1)
 
 
 class Linear(object):
@@ -34,12 +39,19 @@ class Linear(object):
     def updatedWeightAndBias(self):
         weightDerivative = 0.0
         biasDerivative = 0.0
-        for i in range(1):
+        for i in range(len(df)):
             weightDerivative += (-2 * df['Dayofweek'][i] * (df['Close'][i] - self.weight * df['Dayofweek'][i] - self.bias))
             biasDerivative += -2 * (df['Close'][i] - self.weight * df['Dayofweek'][i] - self.bias)
-       
+                   
         self.weight -= (weightDerivative / len(df)) * self.lr
         self.bias -= (biasDerivative / len(df)) * self.lr
+        
+    def normalize(self):
+        for i in df:
+            mean = np.mean(df[i])
+            df[i] -= mean
+            minMax = np.max(df[i]) - np.min(df[i])
+            df[i] /= minMax
             
     def calculateCost(self):
         cost = 0.0
@@ -50,6 +62,7 @@ class Linear(object):
     
     def train(self):
         for i in range(self.n):
+            #self.normalize()
             self.updatedWeightAndBias()
             cost = self.calculateCost()
             self.costList.append(np.sqrt(cost))
@@ -60,18 +73,17 @@ class Linear(object):
             predval = self.weight * actualDayOfWeekVals[i] + self.bias
             self.predictVals.append(predval)
             
-        
     def plotGraph(self):
         plt.plot(self.costList)
         plt.show()
         plt.plot(actualVals)
-        print(len(self.predictVals))
         plt.plot(actualVals[:self.trainDataLen] + self.predictVals)
         plt.show()
 
 trainDataLen = 1000
 df = df[:trainDataLen]
-ravg = Linear(0, 0, 0.4, 446, trainDataLen)
+
+ravg = Linear(0, 0, 0.05, 170, trainDataLen)
 ravg.train()
 ravg.predict()
 ravg.plotGraph()
